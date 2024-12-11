62996a0121f7231a6850385ae696d4ed5d9eab2c
horairahsaleem <horairahsaleem7864@gmail.com>
2024-12-11 21:11:06 +0500

    complete task

--- testController.test.js
+++ testController.test.js
@@ -1,9 +1,9 @@
-const request = require('supertest'); // Import supertest
-const app = require('../app'); // Import your Express app
+const request = require('supertest'); 
+const app = require('../app'); 
 
 describe('GET /tasks', () => {
   it('should return a list of tasks', async () => {
     const response = await request(app).get('/api/v1/tasks');
-    expect(response.statusCode).toBe(200); // Adjust based on your API's response
+    expect(response.statusCode).toBe(200); 
   });
 });
